0540bfb07cb5fa1eb19d12de0d59ce30a728c5d2
Kelly Hays <Kelly.Hays97@gmail.com>
2020-08-24 20:18:36 -0400

    Added selection to sort to cpp file

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 void bubbleSort(vector<int>& a);
+void selectionSort(vector<int>& a);
 void printVector(vector<int> a);
 
 int main()
@@ -20,15 +21,39 @@ int main()
         decreasingInts.push_back(readIn);
     }
 
-    randomInts.close();
+    //randomInts.close();
 
-    //printVector(decreasingInts);
+    printVector(decreasingInts);
 
     bubbleSort(decreasingInts);
 
-    //printVector(decreasingInts);
+    printVector(decreasingInts);
 
-    cout << "The largest element in the array is: " << decreasingInts[0];
+    cout << "The largest element in the array using a bubble sort is: " << decreasingInts[0] << "\n\n";
+
+    vector<int> selectSort;
+
+    //ifstream randomInts("ints.txt");
+
+    randomInts.clear();
+    randomInts.seekg(0);
+
+    readIn = 0;
+
+    while(randomInts >> readIn)
+    {
+     selectSort.push_back(readIn);
+    }
+
+    randomInts.close();
+
+    printVector(selectSort);
+
+    selectionSort(selectSort);
+
+    printVector(selectSort);
+
+    cout << "The largest element in the array using a selection sort is: " << selectSort[0] << "\n\n";
 
     return 0;
 }
@@ -54,6 +79,28 @@ void bubbleSort(vector<int>& a)
             
         }
     }
+}
+
+void selectionSort(vector<int>& a)
+{
+    int i = 0;
+    int j = 0;
+    int minIndex = 0;
+
+    for(i = 0; i < a.size()-1; i++)
+    {
+        minIndex = i;
+        for(j = i + 1; j < a.size(); j++)
+        if(a[j] > a[minIndex])
+        {
+            minIndex  = j;
+        }
+        int temp = a[minIndex];
+        int temp2 = a[i];
+        a[minIndex] = temp2;
+        a[i] = temp;
+    }
+
 }
 
 void printVector(vector<int> a)
